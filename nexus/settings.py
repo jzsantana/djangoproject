05c89fe3d74a80ec81c5b4ff7bc8e421eece67ca
jzsantana <juliass7187@gmail.com>
2023-12-11 14:44:13 -0300

    update: refresh token

--- nexus/settings.py
+++ nexus/settings.py
@@ -167,6 +167,14 @@ REST_FRAMEWORK = {
         # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '3/min',
+        'user': '100/min'
+    },
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 2,
     # 'DEFAULT_THROTTLE_CLASSES':(
     #     'rest_framework.throttling.AnonRateThrottle',
